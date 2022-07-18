d7f39393fb28e9e267244aa3002c587043075a8f
Arooj <arooj.irfan@arbisoft.com>
2022-07-18 11:31:24 +0500

    Applied onclick functionality

--- random_color.js
+++ random_color.js
@@ -2,10 +2,6 @@ const getColor = () => {
   const randomColor = Math.floor(Math.random() * 16777215)
   // To generate random numbers
   const randomCode = '#' + randomColor.toString(16)
-  // To convert to hex codes
   document.getElementById('Bcolor').style.backgroundColor = randomCode
-  // change background color to random colors by clicking
   document.getElementById('Color').innerHTML = randomCode
 }
-document.getElementById('GenColor').addEventListener('click', getColor)
-// after clicking the button having id of GenColor, GetColor function will be called!!
