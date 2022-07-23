8d9cd5a96fd81944bcad771370157ada4d772089
AroojIrfan <aroojirfan@A006-00586.local>
2022-07-23 18:59:38 +0500

    Correct changes

--- weatherApp/weather.js
+++ weatherApp/weather.js
@@ -1,59 +1,33 @@
-
-
-
-
-const weather = async  (city) => {
-    
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=fab8f5d460762f2aefd85455b9b0ff2b")
-   
-    await response.json()
-    .then((data) => showWeather(data));
-    
-    if (!response.ok){
-      alert("incorrect city response");
-    }
-    
-} 
- 
-
-
-
-function  showWeather (data) {
+const weather = async (city) => {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+    const data  = await response.json()
+    showWeather(data)
+  } catch (error) {
+    console.log(error)
+    alert(`Weather info unavailable for this city ${city}`)
+  }
   
-
+}
+showWeather = (data) => {
   const { name } = data;
   const { icon, description } = data.weather[0];
   const { temp, humidity } = data.main;
   const { speed } = data.wind;
-  var.cityName.innerHTML= `${name}`
-  console.log(cityName)
-  document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".humidity").innerText =
-      "Humidity:" + " " + humidity + "%";
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".wind").innerText =
-      "Wind speed :" + " " + speed + "km/hr";
-    document.querySelector(".items").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
-     
-   
-     
+  cityName.innerHTML = `${name}`
+  icon.src =
+    'https://openweathermap.org/img/wn/ + ${icon} + .png';
+  desc.innerText = `${description}`;
+  humidity.innerText =
+    `Humidity:  " "  ${humidity} + %`;
+    temperature.innerText = `${temp}  °C`;
+    windWeather.innerText =
+    `Wind speed :  ${speed} km/hr`;
+  loading.classList.remove("loading");
+  document.body.style.backgroundImage =
+   `url('https://source.unsplash.com/1600x900/? + ${name} + )`;
 }
 
-
-  
-    
-  // if(document.querySelector("#search").value=" "){
-  //   alert(" Invalid City ")
-  // }else{
-  //   weather(document.querySelector("#search").value);
-  // }
-  
-   
-
-function search () {
-  weather(document.querySelector("#search").value);
+function search() {
+  weather(searchitem.value);
 }
